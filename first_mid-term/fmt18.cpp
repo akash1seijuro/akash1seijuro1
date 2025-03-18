a710f7023c2e357973b799bb014c84312ee44640
Акаши Сейджуро <itoshirinuno@gmail.com>
2025-03-18 10:40:22 +0100

    Update fmt18.cpp

--- first_mid-term/fmt18.cpp
+++ first_mid-term/fmt18.cpp
@@ -1,52 +1,55 @@
 #include <iostream>
+#include <algorithm>
+#include <iomanip>
 #include <cstring>
+#include <cctype>
+#include <cmath>
+#include <fstream>
 using namespace std;
 struct SkiLift {
-    char ime[15];
-    int max;
-    bool working;
+    char name[15];
+    int n; //maksimalen broj na korisnici odednash
+    bool valid;
 };
 struct SkiCenter {
-    char ime[20];
-    char drzhava[20];
-    SkiLift skiLifts[20];
-    int n; //broj na ski-liftovi
+    char name[20];
+    char country[20];
+    SkiLift lifts[20];
+    int n;
 };
-void najgolemKapacitet(const SkiCenter *sc, int n) {
-    int max_capacity=0,max_ski_lifts=0,max_capacity_index=0;
+void najgolemKapacitet(SkiCenter *sc, int n) {
+    int index_sc_max=0;
+    int max_capacity=0,max_ski_lifts=0;
     for (int i=0;i<n;i++) {
         int current_capacity=0,current_ski_lifts=0;
         for (int j=0;j<sc[i].n;j++) {
-            if (sc[i].skiLifts[j].working) {
+            if (sc[i].lifts[j].valid) {
+                current_capacity+=sc[i].lifts[j].n;
                 current_ski_lifts++;
-                current_capacity+=sc[i].skiLifts[j].max;
             }
         }
-        if (current_ski_lifts>=max_ski_lifts) {
+        if (current_capacity>max_capacity) {
+            max_capacity=current_capacity;
             max_ski_lifts=current_ski_lifts;
+            index_sc_max=i;
+        }else if (current_capacity==max_capacity) {
+            if (current_ski_lifts>max_ski_lifts) {
+                max_ski_lifts=current_ski_lifts;
+                index_sc_max=i;
+            }
         }
-        if (current_capacity > max_capacity) {
-            max_capacity = current_capacity;
-            max_capacity_index = i;
-        }else if (current_capacity == max_capacity && max_ski_lifts <= current_ski_lifts) {
-            max_ski_lifts = current_ski_lifts;
-            max_capacity_index = i;
-            max_capacity = current_capacity;
-        }
-        //ispechati go ski-centarot so najgolem broj korisnici vo istovreme, ako 2+ se isti togash tojshto ima pogolem broj
-        //ski-liftovi; ime<<endl<<drzhava<<kapacitet (vo predvid samo working=true liftovite
     }
-    cout<<sc[max_capacity_index].ime<<endl<<sc[max_capacity_index].drzhava<<endl<<max_capacity<<endl;
+    cout<<sc[index_sc_max].name<<endl<<sc[index_sc_max].country<<endl<<max_capacity<<endl;
+    //pechati name<<endl<<country<<n na SKiCenter koj ima max korisnici odednash (od site skiliftovi, koi se valid==true), ako se 2+ togash toj koj ima > broj skiliftovi
 }
-int main()
-{
+int main() {
     int n;
     cin>>n;
     SkiCenter sc[n];
-    for (int i = 0; i < n; i++){
-        cin>>sc[i].ime>>sc[i].drzhava>>sc[i].n;
+    for (int i=0;i<n;i++) {
+        cin>>sc[i].name>>sc[i].country>>sc[i].n;
         for (int j=0;j<sc[i].n;j++) {
-            cin>>sc[i].skiLifts[j].ime>>sc[i].skiLifts[j].max>>sc[i].skiLifts[j].working;
+            cin>>sc[i].lifts[j].name>>sc[i].lifts[j].n>>sc[i].lifts[j].valid;
         }
     }
     najgolemKapacitet(sc,n);
