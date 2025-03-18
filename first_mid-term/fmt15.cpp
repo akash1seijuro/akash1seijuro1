cc562d52b5e611dce1b6efb4632664462336c212
Акаши Сейджуро <itoshirinuno@gmail.com>
2025-03-18 10:39:35 +0100

    Update fmt15.cpp

--- first_mid-term/fmt15.cpp
+++ first_mid-term/fmt15.cpp
@@ -1,66 +1,69 @@
 #include <iostream>
+#include <algorithm>
+#include <iomanip>
 #include <cstring>
+#include <cctype>
+#include <cmath>
+#include <fstream>
 using namespace std;
-
-#define DENOVI 5
 #define NEDELI 4
-
+#define DENOVI 5
 struct RabotnaNedela {
-    int casovi[DENOVI];
-    int broj_na_nedela;
+    int casovi[5]; //sekoja rabotna nedela ima svoj broj na rabotni chasovi
+    int nedela; //broj na nedelata
 };
-
 struct Rabotnik {
     char ime[50];
-    RabotnaNedela nedeli[NEDELI];
+    RabotnaNedela nedeli[4];
 };
-
-int maxNedela(Rabotnik r) {
-    int maxCasovi = 0, maxNedelaIndex = 0;
-    for (int i = 0; i < NEDELI; i++) {
-        int suma = 0;
-        for (int j = 0; j < DENOVI; j++) {
-            suma += r.nedeli[i].casovi[j];
+int maxNedela(Rabotnik *r) {
+    int index_rabotnanedela=0;
+    int most_hours=0;
+    for (int i=0;i<4;i++) { //mine niz sekoja RabotnaNedela
+        int current_hours=0;
+        for (int j=0;j<5;j++) { //do 5 bidejki sekoja RabotnaNedela ima max 5 dena vo koja ima rabotni chasovi
+            current_hours+=r->nedeli[i].casovi[j];
         }
-        if (suma > maxCasovi) {
-            maxCasovi = suma;
-            maxNedelaIndex = i;
+        if (current_hours>most_hours) {
+            most_hours=current_hours;
+            index_rabotnanedela=i;
         }
     }
-    return maxNedelaIndex + 1;
+    return index_rabotnanedela+1;//oti se bara da pochnuva od 1, faktichki i=0 kje treba da vrakja 1
+    //ja vrakja RabotnaNedela na rabotnikot *r vo koja ima najgolem broj rabotni chasovi
 }
-
-void table(Rabotnik r[], int n) {
-    cout << "Rab\t1\t2\t3\t4\tVkupno" << endl;
-    for (int i = 0; i < n; i++) {
-        cout << r[i].ime << "\t";
-        int total = 0;
-        for (int j = 0; j < NEDELI; j++) {
-            int weeklyTotal = 0;
-            for (int k = 0; k < DENOVI; k++) {
-                weeklyTotal += r[i].nedeli[j].casovi[k];
+void table(Rabotnik *r, int n) {
+    cout<<"Rab\t"<<"1\t"<<"2\t"<<"3\t"<<"4\t"<<"Vkupno"<<endl;
+    for (int i=0;i<n;i++) {
+        cout<<r[i].ime<<"\t";
+        int max_sum=0;
+        for (int j=0;j<4;j++) {
+            int sum=0;
+            for (int k=0;k<5;k++) {
+                sum+=r[i].nedeli[j].casovi[k];
             }
-            total += weeklyTotal;
-            cout << weeklyTotal << "\t";
+            max_sum+=sum;
+            cout<<sum<<"\t";
         }
-        cout << total << endl;
+        cout<<max_sum;
+        cout<<endl;
     }
 }
-
 int main() {
     int n;
-    cin >> n;
+    cin>>n;
     Rabotnik rabotnici[n];
     for (int i = 0; i < n; ++i) {
-        cin >> rabotnici[i].ime;
+        cin>>rabotnici[i].ime;
         for (int j = 0; j < NEDELI; ++j) {
             for (int k = 0; k < DENOVI; ++k) {
-                cin >> rabotnici[i].nedeli[j].casovi[k];
+                cin>>rabotnici[i].nedeli[j].casovi[k];
             }
         }
     }
-    cout << "TABLE" << endl;
+    cout<<"TABLE"<<endl;
     table(rabotnici, n);
-    cout << "MAX NEDELA NA RABOTNIK: " << rabotnici[n/2].ime << endl << maxNedela(rabotnici[n/2]);
+    cout<<"MAX NEDELA NA RABOTNIK: "<<rabotnici[n/2].ime<<endl;
+    cout<<maxNedela(&rabotnici[n/2]);
     return 0;
 }
