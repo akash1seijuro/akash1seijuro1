e759645cbcb5c43fc639cbe368f404db66d2586d
Акаши Сейджуро <itoshirinuno@gmail.com>
2025-03-20 11:20:16 +0100

    Update fmt1.cpp

--- first_mid-term/fmt1.cpp
+++ first_mid-term/fmt1.cpp
@@ -1,103 +1,88 @@
 #include <iostream>
+#include <algorithm>
+#include <iomanip>
 #include <cstring>
+#include <cctype>
+#include <cmath>
+#include <fstream>
 using namespace std;
 enum tip {
-    POP=0,RAP,ROK
+	POP=0,RAP,ROK
 };
 class Pesna {
 private:
-    char *name;
-    int time;
-    tip type;
+	char *name;
+	int duration;
+	tip type;
+	void copy(const Pesna &p) {
+		name = new char[strlen(p.name)+1];
+		strcpy(name, p.name);
+		duration = p.duration;
+		type = p.type;
+	}
 public:
-    char* getName() {
-        return this->name;
-    }
-    int getTime() {
-        return this->time;
-    }
-    tip getType() {
-        return this->type;
-    }
-    void setName(char *name) {
-        this->name = name;
-    }
-    void setTime(int time) {
-        this->time = time;
-    }
-    void setType(tip type) {
-        this->type = type;
-    }
-    Pesna(const char *name="", int time=0, int type=0) {
-        this->name = new char[strlen(name)+1];
-        strcpy(this->name, name);
-        this->time = time;
-        this->type=(tip)type;
-    }
-    Pesna(const Pesna &p) {
-        this->name = new char[strlen(p.name)+1];
-        strcpy(this->name, p.name);
-        this->time = p.time;
-        this->type = (tip)p.type;
-    }
-    Pesna &operator=(const Pesna &p) {
-        if (this!=&p) {
-            delete[] name;
-            this->name = new char[strlen(p.name)+1];
-            strcpy(this->name, p.name);
-            this->time = p.time;
-            this->type = (tip)p.type;
-        }
-        return *this;
-    }
-    ~Pesna() {
-        delete[] name;
-    }
-    void pecati() {
-        cout<<"\""<<this->name<<"\""<<"-"<<time<<"min"<<endl;
-    }
+	tip getTip() {
+		return type;
+	}
+	int getDuration() {
+		return duration;
+	}
+	Pesna(const char *name="", int duration=0, tip type=POP) {
+		this->name = new char[strlen(name)+1];
+		strcpy(this->name, name);
+		this->duration = duration;
+		this->type = type;
+	}
+	Pesna(const Pesna &p) {
+		copy(p);
+	}
+	Pesna &operator=(const Pesna &p) {
+		if (this!=&p) {
+			delete [] name;
+			copy(p);
+		}
+		return *this;
+	}
+	~Pesna() {
+		delete [] name;
+	}
+	void pecati() {
+		cout<<"\""<<name<<"\"-"<<duration<<"min"<<endl;
+	}
 };
 class CD {
 private:
-    Pesna pesni[10];
-    int number;
-    int duration;
+	Pesna pesni[10];
+	int n;
+	int duration; //maks. dozvoleno vremetraenje na site pesni vo CD-to
 public:
-    int getBroj() {
-        return this->number;
-    }
-    int getDuration() {
-        return this->duration;
-    }
-    void setNumber(int number) {
-        this->number = number;
-    }
-    void setDuration(int duration) {
-        this->duration = duration;
-    }
-    Pesna getPesna(int i) {
-        return pesni[i];
-    }
-    CD(int duration=0,int number=0) {
-        this->duration = duration;
-        this->number = 0;
-    }
-    void dodadiPesna(Pesna p) {
-        int vkupnoVreme = 0;
-        for (int i = 0; i < number; i++) {
-            vkupnoVreme += pesni[i].getTime();
-        }
-        if (number < 10 && vkupnoVreme + p.getTime() <= duration) {
-            pesni[number++] = p;
-        }
-    }
-    void pecatiPesniPoTip(int t) {
-        for (int i = 0; i < number; i++) {
-            if (pesni[i].getType() == t) {
-                pesni[i].pecati();
-            }
-        }
-    }
+	Pesna getPesna(int i) {
+		return pesni[i];
+	}
+	int getBroj() {
+		return n;
+	}
+	CD(int duration=0) {
+		this->duration = duration;
+		n = 0;
+	}
+	void dodadiPesna(Pesna p) {
+		int total=p.getDuration();
+		for (int i=0;i<n;i++) {
+			total+=pesni[i].getDuration();
+		}
+		if (n>=10 || total>duration) {
+			return;
+		}
+		pesni[n++]=p;
+	}
+	void pecatiPesniPoTip(tip t) {
+		for (int i=0;i<n;i++) {
+			if (pesni[i].getTip()==t) {
+				pesni[i].pecati();
+			}
+		}
+	}
 };
 int main() {
 	// se testira zadacata modularno
@@ -158,19 +143,18 @@ int main() {
 
     }
     else if(testCase == 5) {
-        cout << "===== Testiranje na metodot pecatiPesniPoTip() od klasata CD ======" << endl;
-		CD omileno(20);
-		cin>>n;
-			for (int i=0;i<n;i++){
-				cin >> ime;
-				cin >> minuti;
-				cin >> kojtip; //se vnesuva 0 za POP,1 za RAP i 2 za ROK
-				Pesna p(ime,minuti,(tip)kojtip);
-				omileno.dodadiPesna(p);
-			}
-        cin>>kojtip;
-        omileno.pecatiPesniPoTip((tip)kojtip);
-
+	    cout << "===== Testiranje na metodot pecatiPesniPoTip() od klasata CD ======" << endl;
+    	CD omileno(20);
+    	cin>>n;
+    	for (int i=0;i<n;i++){
+    		cin >> ime;
+    		cin >> minuti;
+    		cin >> kojtip; //se vnesuva 0 za POP,1 za RAP i 2 za ROK
+    		Pesna p(ime,minuti,(tip)kojtip);
+    		omileno.dodadiPesna(p);
+    	}
+    	cin>>kojtip;
+    	omileno.pecatiPesniPoTip((tip)kojtip);
     }
 
 return 0;
