32f1b236c056c73c9d42699c11f313857c4e6c47
Акаши Сейджуро <itoshirinuno@gmail.com>
2025-04-09 14:08:32 +0200

    Add files via upload

--- lab3/lab3-1.cpp
+++ lab3/lab3-1.cpp
@@ -1,159 +1,115 @@
 #include <iostream>
 #include <cstring>
 using namespace std;
-class Mountain;
-class MountainTrail;
-class MountainTrail {
+class Scholarship;
+class Student;
+class Student {
 private:
     char name[51];
-    int length;
-    double slope;
+    char surname[51];
+    char faculty[51];
+    float gpa;
+    static int ALL;
+    static int FINKASHI;
 public:
-    friend class Mountain;
-    friend void sortMountainsByPeakElevation(Mountain mountains[], int n);
-    MountainTrail(const char *name="", int length=0, double slope=0.0) {
-        strcpy(this->name,name);
-        this->length=length;
-        this->slope=slope;
+    friend class Scholarship;
+    friend void studentsWithScholarship(Scholarship &s, int n);
+    Student(const char *name="", const char *surname="", const char *faculty="", float gpa=0) {
+        strcpy(this->name, name);
+        strcpy(this->surname, surname);
+        strcpy(this->faculty, faculty);
+        this->gpa = gpa;
+        ALL++;
+        if (strcmp(faculty,"FINKI")==0 && gpa>9.0) {
+            FINKASHI++;
+        }
     }
-    MountainTrail(const MountainTrail &other) {
-        strcpy(this->name,other.name);
-        this->length=other.length;
-        this->slope=other.slope;
+    ~Student(){}
+    bool finkiStudent() const{
+        return (strcmp(faculty,"FINKI")==0 && gpa>9.0);
     }
-    double difficulty() const{
-        return length*slope*1.0/100.0;
-    }
-    void display() const {
-        cout<<name<<" "<<length<<" "<<slope<<endl;
+    void print() const{
+        cout<<name<<" "<<surname<<" "<<faculty<<" "<<gpa<<endl;
     }
 };
-class Mountain {
+int Student::ALL = 0;
+int Student::FINKASHI = 0;
+class Scholarship {
 private:
     char name[51];
-    MountainTrail trails[5];
-    int peak_elevation;
-    static int NUM_MOUNTAINS;
+    Student students[100];
+    int n;
+    static float GPA; //zbir od site proseci na FINKASHI samo !!!
 public:
-    friend void sortMountainsByPeakElevation(Mountain mountains[], int n);
-    static int getNumMountains() {
-        return NUM_MOUNTAINS;
+    friend void studentsWithScholarship(Scholarship &s, int n);
+    Scholarship(const char *name="") {
+        strcpy(this->name, name);
+        n=0;
     }
-    Mountain(const char *name="", MountainTrail *trails=nullptr, int peak_elevation=0) {
-        strcpy(this->name,name);
-        if (trails) {
-            for (int i=0;i<5;i++) {
-                this->trails[i]=trails[i];
+    ~Scholarship(){}
+    void addStudent(const Student &s) {
+        if (n<100) {
+            students[n++]=s;
+            if (s.finkiStudent()) {
+                GPA+=s.gpa;
             }
         }
-        this->peak_elevation=peak_elevation;
-        NUM_MOUNTAINS++;
     }
-    ~Mountain() {
-        NUM_MOUNTAINS--;
-    }
-    void display() const {
-        cout<<name<<": "<<peak_elevation<<"m"<<endl;
+    void print() const {
+        cout<<"Scholarship name: "<<name<<", total applicants: "<<n<<endl;
     }
 };
-void sortMountainsByPeakElevation(Mountain mountains[], int n) {
+float Scholarship::GPA = 0.0;
+void studentsWithScholarship(Scholarship &s, int n) {
     for (int i=0;i<n;i++) {
         for (int j=0;j<n-1-i;j++) {
-            if (mountains[j].peak_elevation<mountains[j+1].peak_elevation) {
-                swap(mountains[j],mountains[j+1]);
+            if (s.students[j].gpa<s.students[j+1].gpa) {
+                swap(s.students[j],s.students[j+1]);
             }
         }
     }
+    cout<<"FINKI students with a scholarship"<<endl;
+    int count=0;
     for (int i=0;i<n;i++) {
-        mountains[i].display();
+        if (s.students[i].finkiStudent() && count<3) {
+            count++;
+            s.students[i].print();
+        }
     }
+    cout<<"Scholarship data shows that "<<Student::FINKASHI*100.0/n<<"% of applicants are from FINKI, with an average grade of "<<Scholarship::GPA/Student::FINKASHI*1.0<<endl;
 }
-int Mountain::NUM_MOUNTAINS = 0;
-
 int main() {
-
-    int test_case_n;
-
-    char trail_name[100];
-    int trail_length;
-    double trail_slope;
-
-    char mountain_name[100];
-    int mountain_peak_elevation;
-
-    cin>>test_case_n;
-    if (test_case_n == 0) {
-        cout << "Testing MountainTrail c-tors, display function:"<<endl;
-        cin >> trail_name >> trail_length >> trail_slope;
-        MountainTrail mt = MountainTrail(trail_name, trail_length, trail_slope);
-        MountainTrail mt2 = MountainTrail(mt);
-        mt.display();
-        mt2.display();
-    } else if (test_case_n == 1) {
-        cout << "Testing MountainTrail difficulty function:"<<endl;
-        for (int i = 0; i < 5; ++i) {
-            cin >> trail_name >> trail_length >> trail_slope;
-            MountainTrail mt = MountainTrail(trail_name, trail_length, trail_slope);
-            cout<<mt.difficulty()<<endl;
-        }
-    } else if (test_case_n == 2) {
-        cout << "Testing Mountain c-tor, display function:"<<endl;
-        MountainTrail trails[5];
-
-        cin>>mountain_name>>mountain_peak_elevation;
-
-        for (int i = 0; i < 5; ++i) {
-            cin >> trail_name >> trail_length >> trail_slope;
-            trails[i] = MountainTrail(trail_name, trail_length, trail_slope);
-        }
-
-        Mountain mountain = Mountain(mountain_name, trails, mountain_peak_elevation);
-        mountain.display();
-    } else if (test_case_n == 3) {
-        cout << "Testing static field in Mountain:" <<endl;
-        MountainTrail trails[5];
-
-        cin>>mountain_name>>mountain_peak_elevation;
-
-        for (int i = 0; i < 5; ++i) {
-            cin >> trail_name >> trail_length >> trail_slope;
-            trails[i] = MountainTrail(trail_name, trail_length, trail_slope);
-        }
-
-        for (int i = 0; i < 10; ++i) {
-            Mountain mountain = Mountain(mountain_name, trails, mountain_peak_elevation);
-        }
-
-        Mountain m1 = Mountain(mountain_name, trails, mountain_peak_elevation);
-        Mountain m2 = Mountain(mountain_name, trails, mountain_peak_elevation);
-        Mountain m3 = Mountain(mountain_name, trails, mountain_peak_elevation);
-        Mountain m4 = Mountain(mountain_name, trails, mountain_peak_elevation);
-        Mountain m5 = Mountain(mountain_name, trails, mountain_peak_elevation);
-
-        if(Mountain::getNumMountains() == 5) {
-            cout<<"OK";
-        } else {
-            cout<<"Missing mountain count increment/decrement";
-        }
-    } else if (test_case_n == 4) {
-        int M;
-        cin>>M;
-        cout<<"Testing order function:"<<endl;
-
-        Mountain mountains[M];
-
-        for (int i = 0; i < M; ++i) {
-            cin>>mountain_name>>mountain_peak_elevation;
-            MountainTrail trails[5];
-
-            for (int j = 0; j < 5; ++j) {
-                cin >> trail_name >> trail_length >> trail_slope;
-                trails[j] = MountainTrail(trail_name, trail_length, trail_slope);
-            }
-
-            mountains[i] = Mountain(mountain_name, trails, mountain_peak_elevation);
-        }
-
-        sortMountainsByPeakElevation(mountains, M);
+    char name[50], surname[50], faculty[50], scholarship_name[50];
+    float average_grade;
+    int n,case_;
+    cin>>case_;
+    if (case_ == 0)
+        //Testing Student constructor
+    {
+        cout<<"--Testing finkiStudent method--"<<endl;
+        Student s("Ana", "Denkova", "BAS", 9.89);
+        cout<<s.finkiStudent();
     }
+    else if (case_==1){
+        //Testing addStudent and print method
+        cout<<"--Testing addStudent and print method--"<<endl;
+        Scholarship sc("ITCompany");
+        Student s("Ana", "Denkova", "BAS", 9.89);
+        sc.addStudent(s);
+        sc.print();
+    }
+    else if (case_ == 2){
+        cin>>scholarship_name;
+        Scholarship sc(scholarship_name);
+        cin>>n;
+        for(int i=0; i<n; i++)
+        {
+            cin>>name>>surname>>faculty>>average_grade;
+            Student s = Student(name, surname, faculty, average_grade);
+            sc.addStudent(s);
+        }
+        sc.print();
+        studentsWithScholarship(sc,n);
+    }
+    return 0;
 }
